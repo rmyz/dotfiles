e0259bb5e83c49f757d2977782be055b1c913adc
Sergi Romeu <romeuhd@gmail.com>
2023-06-27 09:25:03 +0200

    add more defaults

--- mac/installer.sh
+++ mac/installer.sh
@@ -3,10 +3,13 @@
 # Disables natural scroll direction
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool FALSE
 # Shows percentage on battery
-defaults write com.apple.menuextra.battery ShowPercent YES
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
 # Disables recent apps on dock
 defaults write com.apple.dock show-recents -bool false
+# Remove hot corners
+defaults write com.apple.dock wvous-br-corner -int 1
+# Disable startup sound
+sudo nvram StartupMute=%01
 
 # Kills processes in order to show changes
 killall Dock
-killall SystemUIServer
