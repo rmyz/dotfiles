86229f7e9f55ecd3625efe0de778f41914430fac
Sergi Romeu <romeuhd@gmail.com>
2024-06-01 22:31:50 +0200

    feat: remove spotlight hotkey

--- mac/installer.sh
+++ mac/installer.sh
@@ -8,6 +8,9 @@ defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Batter
 defaults write com.apple.dock show-recents -bool false
 # Remove hot corners
 defaults write com.apple.dock wvous-br-corner -int 1
+# Removes Spotlight hotkey Command+Space
+/usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist -c \
+  "Add :AppleSymbolicHotKeys:64:enabled bool false"
 # Disable startup sound
 sudo nvram StartupMute=%01
 
